b97fdd0481a61520c3a5aa5820ae092bad891913
KenWoo <kenwoov@outlook.com>
2020-01-31 15:17:23 +0800

    add no. 121

---
+++ LeetCode/easy/121. Best Time to Buy and Sell Stock/answer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices):
+        if prices is None or len(prices) < 2:
+            return 0
+        max = 0
+        min = prices[0]
+        for p in prices:
+            if p < min:
+                min = p
+            else:
+                if max < p - min:
+                    max = p - min
+
+        return max
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.maxProfit([7, 1, 5, 3, 6, 4])
+    print(result)
