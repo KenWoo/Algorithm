2f50840bbdf5d7ad20492b47046660036243f5a3
KenWoo <kenwoov@outlook.com>
2020-01-31 14:03:50 +0800

    add no. 167

---
+++ LeetCode/easy/167. Two Sum II - Input array is sorted/answer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        f_index = 0
+        s_index = len(numbers) - 1
+        while f_index < s_index:
+            sum = numbers[f_index] + numbers[s_index]
+            if sum == target:
+                return [f_index + 1, s_index + 1]
+            elif sum < target:
+                f_index += 1
+            else:
+                s_index -= 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.twoSum([2, 7, 11, 15], 9)
+    print(result)
