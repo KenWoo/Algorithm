99b0e3c62ff0c84251166ce180ce173c331e9dd6
Ken.W <kenwoov@outlook.com>
2020-02-29 09:16:54 +0800

    add no.442

---
+++ Algorithms/Medium/442. Find All Duplicates in an Array/answer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            idx = abs(nums[i]) - 1
+            if nums[idx] < 0:
+                res.append(idx+1)
+            else:
+                nums[idx] *= -1
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.findDuplicates([4, 3, 2, 7, 8, 2, 3, 1])
+    print(result)
