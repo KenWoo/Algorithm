a7737752ca26a9c35a1d84533c492b63ebfba21e
Ken.W <kenwoov@outlook.com>
2020-05-20 07:33:36 +0800

    add no.704

---
+++ Algorithms/Easy/704. Binary Search/answer.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r-l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return - 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.search([-1, 0, 3, 5, 9, 12], 9)
+    print(result)
