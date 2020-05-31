9769884274298ac003c13c4c906e732393810967
Ken.W <kenwoov@outlook.com>
2020-05-31 08:47:57 +0800

    add no.720

---
+++ Algorithms/Easy/720. Longest Word in Dictionary/answer.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        wordset = set(words)
+        words.sort(key=lambda c: (-len(c), c))
+        for word in words:
+            if all(word[:k] in wordset for k in range(1, len(word))):
+                return word
+        return ''
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.longestWord(
+        ["a", "banana", "app", "appl", "ap", "apply", "apple"])
+    print(result)
