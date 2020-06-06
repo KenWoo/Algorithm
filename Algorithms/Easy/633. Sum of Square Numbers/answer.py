538171a239ba475a14a8169add72c8a1672ca4dc
Ken.W <kenwoov@outlook.com>
2020-06-06 09:42:09 +0800

    add no.633

---
+++ Algorithms/Easy/633. Sum of Square Numbers/answer.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        i = 2
+        while i * i <= c:
+            count = 0
+            if c % i == 0:
+                while c % i == 0:
+                    count += 1
+                    c //= i
+                if i % 4 == 3 and count % 2 != 0:
+                    return False
+            i += 1
+        return c % 4 != 3
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.judgeSquareSum(5)
+    print(result)
