f5104383707868bca6bba7f681cf06d60f63e18e
Ken.W <kenwoov@outlook.com>
2020-06-15 19:58:47 +0800

    add no.647

---
+++ Algorithms/Medium/647. Palindromic Substrings/answer.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        N = len(s)
+        res = 0
+        for center in range(2*N-1):
+            left = center // 2
+            right = left + center % 2
+            print(left, right)
+            while left >= 0 and right < N and s[left] == s[right]:
+                res += 1
+                left -= 1
+                right += 1
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.countSubstrings("aaa")
+    print(result)
