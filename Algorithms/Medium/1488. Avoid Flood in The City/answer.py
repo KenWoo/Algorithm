444c95df329eb920493a1225ad110960f1a411c7
Ken.W <kenwoov@outlook.com>
2020-06-22 23:09:14 +0800

    update 1488

--- Algorithms/Medium/1488. Avoid Flood in The City/answer.py
+++ Algorithms/Medium/1488. Avoid Flood in The City/answer.py
@@ -1,8 +1,35 @@
 from typing import List
+import collections
+import heapq
+
 
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
-        
+        N = len(rains)
+        dic = collections.defaultdict(list)
+        res = [-1] * N
+        to_empty = []
+
+        for i, lake in enumerate(rains):
+            dic[lake].append(i)
+
+        for i in range(N):
+            lake = rains[i]
+            if lake:
+                if dic[lake] and dic[lake][0] < i:
+                    return []
+                if dic[lake] and len(dic[lake]) > 1:
+                    heapq.heappush(to_empty, dic[lake][1])
+            else:
+                if to_empty:
+                    res[i] = rains[heapq.heappop(to_empty)]
+                    dic[res[i]].pop(0)
+                else:
+                    res[i] = 1
+        return res
+
 
 if __name__ == "__main__":
-    pass
+    s = Solution()
+    result = s.avoidFlood([1, 2, 0, 0, 2, 1])
+    print(result)
