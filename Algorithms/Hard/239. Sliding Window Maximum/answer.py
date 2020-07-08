dca0f8fb639855c8f422860745db9f7e8cf3061b
Ken.W <kenwoov@outlook.com>
2020-07-08 10:27:49 +0800

    add no.239

---
+++ Algorithms/Hard/239. Sliding Window Maximum/answer.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        window, res = [], []
+        for i, n in enumerate(nums):
+            if i >= k and window[0] <= i - k:
+                window.pop(0)
+            while window and nums[window[-1]] <= n:
+                window.pop()
+            window.append(i)
+            if i >= k - 1:
+                res.append(nums[window[0]])
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)
+    print(result)
