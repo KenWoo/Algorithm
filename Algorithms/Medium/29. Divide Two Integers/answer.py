d0388b2fd92b4ae04bafd5823e7638278175d880
Ken.W <kenwoov@outlook.com>
2020-07-15 10:52:16 +0800

    add no.29

---
+++ Algorithms/Medium/29. Divide Two Integers/answer.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        res = flag = 0
+        if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+            flag = 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        while dividend >= divisor:
+            n = 0
+            while dividend >= divisor << n:
+                n += 1
+            res += 1 << (n-1)
+            dividend -= (divisor << (n-1))
+        res = -res if flag == 1 else res
+        if res < -2147483648 or res > 2147483647:
+            return 2147483647
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    result = s.divide(10, 3)
+    print(result)
